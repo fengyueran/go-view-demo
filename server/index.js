d124360c6d07dcf3e7b07d43036d1a851cc087c5
xinghunm <xinghunm@curacloudcorp.com>
2023-08-16 10:28:16 +0800

    update server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
 
 const random = (m, n) => Math.ceil(Math.random() * (n - m)) + m
 
-app.get('/get-data', (req, res) => {
-  res.send({ data: { temperature: random(10, 70) } })
+app.get('/dataset', (req, res) => {
+  res.send({ data: { voltage: random(10, 70), soc: random(0, 100) } })
+})
+
+app.post('/execute', (req, res) => {
+  console.log(req.body)
+  const { code } = req.body
+  // const a = eval(code);
+  const result = new Function(code)()
+  res.send({ data: { voltage: random(10, 70), soc: random(0, 100) } })
 })
 
 app.listen(8080, () => console.log('listening on port 8080'))
